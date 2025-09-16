8ee82fcd2bfb5f3a4ccdbf7755dadd5249191f68
CIP Chimbote <informatica@cip.org.pe>
2025-09-16 12:01:18 -0500

    cambio pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function paginaPrincipal() {
   return (
     <div className='text-center '>
       
-        ARBITRAJE 2
+      <div className='bg-blue-800 p-4 font-bold text-white '>
+
+          CENTRO  DE ARBITRAJE
+
+      </div>
+
+        
 
     </div>
   )
